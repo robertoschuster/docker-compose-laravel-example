dae6e58e2cda8878c74aaef1dc0e8d003c085b06
Roberto Schuster <roberto@e-get.com.br>
2023-01-22 20:54:23 -0300

    improvement: add order by in the select of places

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-  $togo = DB::select('select * from places where visited = ?', [0]);
-  $visited = DB::select('select * from places where visited = ?', [1]);
+  $togo = DB::select('select * from places where visited = ? order by name', [0]);
+  $visited = DB::select('select * from places where visited = ? order by name', [1]);
 
   return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
 });
